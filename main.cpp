5e4bdd71240faf61e4c6688fbb31def77a8f6ee2
clemo <xenjamo@gmail.com>
2022-11-07 15:37:42 +0100

    adiu

--- main.cpp
+++ main.cpp
@@ -71,18 +71,14 @@ int main()
     uint8_t len = sizeof(data);
     uint8_t rx_len = 0;
     uint8_t tx_len = 0;
-    uint8_t* rtcm_data = (uint8_t*)calloc(MAXIMUM_BUFFER_SIZE*MAXIMUM_MESSAGES,1);
+    uint8_t* rtcm_data = (uint8_t*)malloc(MAXIMUM_RTCM_MESSAGE_LENGTH*MAXIMUM_RTCM_MESSAGES); //ideally this is dynamic but this should suffice
     uint16_t rtcm_len = 0;
+    uint8_t* ubx_data = (uint8_t*)malloc(300);
+    uint16_t ubx_len = 0;
     rtk_state state = RTK_IDLE;
     uint8_t loop = 1;
     
     uint8_t n = 0;
-    //uint8_t i = 0;
-    
-    uint8_t* buffer = NULL;
-    uint16_t buf_len = MAXIMUM_BUFFER_SIZE*MAXIMUM_MESSAGES;
-    buffer = (uint8_t*)calloc(buf_len, 1);
-
 
     led = 0;
 
@@ -117,13 +113,26 @@ int main()
                         //printf("n=%d\n",lora.n_payloads);
                         state = RTK_RECEIVE;
                     } else {
-                        
+
+                        memcpy(gps.rtcm_msg, rtcm_data, rtcm_len);
+                        if(!gps.decode()){
+                            printf("decode failed n = %d\n",gps.msg_ready(RTCM));
+                        }
+                        n = gps.msg_ready(RTCM);
+                        printf("n = %d\n", n);
+                        for(int i = 0; i < n; i++){
+                        printf("len%d = %d,%d\n", i, gps.msg[i].length, gps.msg[i].type);
+                        }
+                        gps.clearAll();
                         state = RTK_SEND_RTCM_MSG;
+                        /*
                         printf("rtcm msg = ");
                         for(int i = 0; i < rtcm_len; i++){
                             printf("%02x", rtcm_data[i]);
                         }
+                        */
                         printf("\n\n");
+                        
                         
                     }
                     //ThisThread::sleep_for(500ms);
@@ -143,6 +152,7 @@ int main()
                     lora.n_payloads = data[4];
                     data[0] = 0;
                     printf("n=%d, buf=%d, rx=%d\n", lora.n_payloads, rtcm_len, rx_len-5);
+                    printf("some signal str: SNR=%d, RSSI=%d\n", lora.getSNR(),lora.getRSSI());
                     lora.flags = lora.flags | 0x01;
                     lora.setModeIdle();
                     if(!lora.transmit(data, 0)){ //transmit data
@@ -166,17 +176,64 @@ int main()
             case(RTK_GET_RTCM_MSG): //read uart
                 //printf("congrats! no crash");
                 
-                if(gps.readCompleteMsg(rtcm_data, rtcm_len)){
-                    printf("rx = 0x");
-                    print_hex((char*)rtcm_data, rtcm_len);
+                if(gps.data_ready()){
+                    
+                    /*
+                    printf("cpl = 0x");
+                    print_hex((char*)gps.rtcm_msg, gps.rtcm_msg_pointer);
+                    printf(" %d bytes\n", gps.rtcm_msg_pointer);
+                    */
+
+                    gps.decode();
+                    gps.printMsgTypes();
+                    gps.encode_RTCM(rtcm_data, rtcm_len);
+                    //gps.encode_UBX(ubx_data, ubx_len);
+                    char buf[200];
+                    uint16_t l = 0;
+                    gps.ubx[0].ubx2string(buf, l);
+                    
+
+                    printf("ubx = 0x");
+                    //print_hex((char*)ubx_data, ubx_len);
+                    printf(" %d bytes\n", ubx_len);
+                    printf("rtcm = 0x");
+                    //print_hex((char*)rtcm_data, rtcm_len);
                     printf(" %d bytes\n", rtcm_len);
                     
-                    
-                    printf("\n");
+
+
+
+                    printf("total bytes from UART = %d\n", gps.rtcm_msg_pointer);
+                    gps.clearAll();
+                    /*
+                    printf("rx = 0x");gps.getRtcmMsg(rtcm_data, rtcm_len)
+                    print_hex((char*)rtcm_data, rtcm_len);
+                    printf(" %d bytes\n", rtcm_len);
                     
                     print_hex((char*)gps.rtcm_msg,gps.rtcm_msg_pointer);
                     printf("\n\n");
                     //led = !led;
+                    */
+
+                    //maybe pack this in a function
+                    lora.n_payloads = lora.get_n_payloads(rtcm_len);
+                    if(lora.n_payloads){
+                        tx_len = RH_RF95_MAX_MESSAGE_LEN;
+                        rtcm_len -= tx_len;
+                    } else {
+                        tx_len = rtcm_len % RH_RF95_MAX_MESSAGE_LEN;
+                        rtcm_len = 0;
+                    }
+                    printf("n=%d, tx=%d, buf=%d\n",lora.n_payloads, tx_len, rtcm_len);
+                    /////function end
+                    if(!lora.transmit(rtcm_data, tx_len)){ //transmit data
+                        printf("transmit failed\n");
+                        state = RTK_ERR;
+                        break;
+                    }
+                    led = 1;
+                    state = RTK_TRANSMIT;
+                    
                 } else{
                     //no message
                 }
@@ -189,6 +246,7 @@ int main()
                 //ThisThread::sleep_for(200ms);
             break;
             case(RTK_SEND_RTCM_MSG):
+                
                 gps.writeCompleteMsg(rtcm_data, rtcm_len);
                 state = RTK_RECEIVE;
                 led = 0;
